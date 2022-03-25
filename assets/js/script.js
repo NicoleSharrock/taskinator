e01ade89abfbf08d351173ffcd62891537f8f0cc
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-03-25 13:25:17 -0400

    creating arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksInProgressEl = document.querySelector("#task-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelector("#page-content");
+var tasks = updatedTaskArr;
+
 
 var taskFormHandler = function (event) {
     event.preventDefault();
@@ -30,10 +32,13 @@ var taskFormHandler = function (event) {
     } else {
         var taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
+            type: taskTypeInput,
+            status: "to do"
         };
 
         createTaskEl(taskDataObj);
+        console.log(taskDataObj)
+        console.log(taskDataObj.status);
     }
 };
 
@@ -47,13 +52,16 @@ var createTaskEl = function (taskDataObj) {
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
+    taskDataObj.id = taskIdCounter;
+
+    tasks.push(taskDataObj);
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
     tasksToDoEl.appendChild(listItemEl);
 
     // increase task counter for next unique id
-    taskInfoEl++;
+    taskIdCounter++;
 };
 
 var createTaskActions = function (taskId) {
@@ -103,6 +111,14 @@ var completeEditTask = function (taskName, taskType, taskId) {
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
 
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].name = taskName;
+            tasks[i].type = taskType;
+        }
+    };
+    debugger
+
     alert("Task Updated!");
 
     // remove data attribute from form
@@ -144,9 +160,18 @@ var taskStatusChangeHandler = function (event) {
     } else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
+
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].status == statusValue;
+        }
+    }
 };
 
 var editTask = function (taskId) {
+    console.log(taskId);
+
+
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
 
     //get content from task name and type 
@@ -169,6 +194,14 @@ var deleteTask = function (taskId) {
     console.log(taskId);
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+
+    var updatedTaskArr = [];
+
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id !== parseInt(taskId)) {
+            updatedTaskArr.push(tasks[i]);
+        }
+    }
 };
 
 // create a new task
